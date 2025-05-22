90624db123824377b155860435650d8d368f46e8
minhpa <minhpa@vnpost.vn>
2025-05-22 11:11:39 +0700

    update database

--- src/main/java/com/example/dailychatbot/controller/ExpenseController.java
+++ src/main/java/com/example/dailychatbot/controller/ExpenseController.java
@@ -7,6 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,12 +20,21 @@ public class ExpenseController {
     @Autowired
     ExpenseService expenseService;
 
-    @PostMapping()
+    @PostMapping
     public ResponseEntity<ExpenseResponse> onUpdateReceived(@RequestBody TelegramMessage telegramMessage) {
+        if (telegramMessage == null) {
+            log.error("Received null TelegramMessage!");
+            return ResponseEntity.badRequest().build();
+        }
         ExpenseResponse response = expenseService.saveFromTelegram(telegramMessage);
-        System.out.println("telegramMessage: " + telegramMessage);
+        if (response == null) {
+            log.warn("ExpenseResponse from service is null!");
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+        log.info("Processed TelegramMessage: {}", telegramMessage);
         return ResponseEntity.ok(response);
     }
+
 
     @GetMapping()
     public ResponseEntity<List<ExpenseResponse>> getAll() {
